baa420b380e37e53337cc39e711312254f1c5f3f
juze117 <juzengler@gmail.com>
2020-11-15 15:21:06 +0100

    rem unused imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { AppLoading } from 'expo';
