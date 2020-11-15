339b8cfc32c0859d721a790526d0667a68433a79
juze117 <juzengler@gmail.com>
2020-11-15 16:03:01 +0100

    rem unused imports

--- screens/NutrientDetailScreen.js
+++ screens/NutrientDetailScreen.js
@@ -1,12 +1,9 @@
-import React, { useState, useReducer, useCallback, useEffect, useRef } from 'react'
-import { ScrollView, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity, Text, Button, ActivityIndicator, Alert, Image, Dimensions } from 'react-native'
-import { fetchUserData } from '../store/actions/auth'
-import { fetchIngredients } from '../store/actions/nutrition'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { View, Text, Dimensions } from 'react-native'
+import { useDispatch } from 'react-redux'
 
 //Defining HomeScreen functional component
 const NutrientDetailScreen = () => {
-  const dispatch = useDispatch()
 
   //Returning the JSX code
   return(
