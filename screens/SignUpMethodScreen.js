14c0641c62738fb322bb7b21d27103ef71dce2ac
juze117 <juzengler@gmail.com>
2020-11-15 16:08:54 +0100

    rem unused imports

--- screens/SignUpMethodScreen.js
+++ screens/SignUpMethodScreen.js
@@ -1,5 +1,5 @@
-import React, { useState, useReducer, useCallback, useEffect, useRef } from 'react'
-import { ScrollView, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity, Text, Button, ActivityIndicator, Alert, Image, Dimensions } from 'react-native'
+import React from 'react'
+import { View, StyleSheet, TouchableOpacity, Text, Dimensions } from 'react-native'
 import { Ionicons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import COLORS from '../constants/Colors'
 
